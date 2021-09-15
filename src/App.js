ffa7a58e910f0ea813bc832d4b4c81ab39279b8c
Sergii Anikin <sergeant.ua@gmail.com>
2021-09-15 04:56:50 +0300

    add tracks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import { Provider } from './context';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <>
+            <Navbar/>
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
